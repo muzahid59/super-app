cf184b14273e55f2b3d428968bdf38ce1b9dbe6b
muzahid59 <mmsaddam@gmail.com>
2026-04-21 16:41:25 +0600

    feat: wire Firebase auth into main.dart with auth-gated home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'providers/auth_provider.dart';
 import 'providers/transaction_provider.dart';
+import 'repositories/firebase/firestore_session_repository.dart';
+import 'repositories/firebase/firestore_user_repository.dart';
+import 'screens/login_screen.dart';
+import 'screens/otp_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/review_edit_screen.dart';
 import 'screens/transaction_list_screen.dart';
 import 'screens/camera_capture_screen.dart';
-import 'screens/review_edit_screen.dart';
+import 'services/auth/firebase/firebase_auth_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -14,25 +25,72 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => TransactionProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(
+            authService: FirebaseAuthService(),
+            userRepo: FirestoreUserRepository(),
+            sessionRepo: FirestoreSessionRepository(),
+          ),
+        ),
+        ChangeNotifierProvider(create: (_) => TransactionProvider()),
+      ],
       child: MaterialApp(
         title: 'Receipt Scanner',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF2196F3),
-          ),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2196F3)),
           useMaterial3: true,
         ),
-        initialRoute: '/',
+        home: const _AuthGate(),
         routes: {
-          '/': (context) => const TransactionListScreen(),
           '/capture': (context) => const CameraCaptureScreen(),
           '/review': (context) => const ReviewEditScreen(),
           '/edit': (context) => const ReviewEditScreen(),
         },
       ),
+    );
+  }
+}
+
+class _AuthGate extends StatefulWidget {
+  const _AuthGate();
+
+  @override
+  State<_AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<_AuthGate> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<AuthProvider>().checkSession();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthProvider>(
+      builder: (context, auth, _) {
+        switch (auth.state) {
+          case AuthState.authenticated:
+            return const TransactionListScreen();
+          case AuthState.otpSent:
+            return const OtpScreen();
+          case AuthState.newUser:
+            return const RegisterScreen();
+          case AuthState.initial:
+          case AuthState.loading:
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          case AuthState.unauthenticated:
+          case AuthState.error:
+            return const LoginScreen();
+        }
+      },
     );
   }
 }
